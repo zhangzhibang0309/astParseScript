6b24dd017ac33aaeb32e8ffe3255a84e84143bf4
zhangzhibang <zhangzhibang@exiao.tech>
2022-07-15 10:16:03 +0800

    fix(fix 改正错误): 改正之前对访问者类的错误遍历写法

--- index.js
+++ index.js
@@ -11,19 +11,32 @@ const root = "../pc-fe";
 // 结果索引
 let i = 1;
 
+/**
+ * 这个是一个遍历项目目录的一个函数，大概就是能够遍历到所有文件，然后拿到文件的文件名 扩展名 路径信息等
+ * 然后里面有两个回调
+ * 第一个回调 return为true则调用第二个回调
+ * 第二个回调 就是对每个文件进行处理 核心逻辑都在下面
+ */
 walkProject(
   root,
   (fileInfo) => fileInfo.ext === ".html" && !fileInfo.name.includes("index"),
   (fileInfo) => {
+    /**
+     * 就是这个 比如这里是把文件里的code拿到 解析成了ast 然后使用访问者模式去遍历这个ast
+     */
     let code = fs.readFileSync(fileInfo.path).toString();
 
     const filePath = fileInfo.path.replace(root, "");
     const visitor = new Visitor(filePath);
     const result = new HtmlParser().parse(code, "batch");
 
-    // 将某一个html文件里面的ast rootNodes进行递归，然后去判断每个元素是否符合我们的要求，将符合要求的加入visitor对象的results里面
+    /**
+     * 这个就是我说的访问者 @angular/compiler实现了一个父类 可以访问所有的element 访问所有的属性attribute等等还有好多
+     * 功能就是可以很方便的递归遍历所有的ast节点的某个属性 或者每个元素
+     */
     visitAll(visitor, result.rootNodes);
 
+    // 业务逻辑 忽略
     let res = visitor.results.map((item) => `${item}`).join("\n");
     if (res) {
       console.log(res);
@@ -41,6 +54,11 @@ class Visitor extends RecursiveVisitor {
     this.file = file;
   }
 
+  /**
+   * 这个就是访问拿到的ast的所有元素 因为是html 就是html的所有元素 里面包括很多信息 包括属性
+   * @param {*} el el就是每个元素的ast 因为父类都写好了 这里直接用
+   * 但是ast 是一层一层的 有children 所以你本身是要递归调用的 但是这个访问者就很爽 帮你实现了 你只需要在调用一次 super.visitElement(el) 就可以不断递归完所有
+   */
   visitElement(el) {
     el.children = this.removeEmptyText(el.children);
 
@@ -66,8 +84,11 @@ class Visitor extends RecursiveVisitor {
         }
       }
     }
+
+    super.visitElement(el);
   }
 
+  // 下面的函数都可以不看 因为是我这个任务的具体逻辑
   // 删除children中的空Text节点，例如换行和锁进
   removeEmptyText(nodes) {
     return nodes.filter((node) =>
